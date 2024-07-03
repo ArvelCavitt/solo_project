f2668a3401fcacd4c71ca9d4957376ff9bf597c1
Arvel Cavitt <arvelcavitt4@gmail.com>
2024-07-02 23:38:53 -0700

    commit

---
+++ flask_app/models/completed_workout.py
@@ -0,0 +1,30 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class CompletedWorkout:
+    db = "fitness"
+
+    def __init__(self,data):
+        self.id = data['id']
+        self.user_id = data['user_id']
+        self.training_id = data['training_id']
+        self.completed_at = data['completed_at']
+
+    @classmethod
+    def complete_workout(cls,data):
+        query = "INSERT INTO completed_workouts (user_id, training_id, completed_at) VALUES (%(user_id)s, %(training_id)s, NOW())"
+        return connectToMySQL(cls.db).query_db(query,data)
+    
+    @classmethod
+    def get_completed_workouts_by_user(cls, data):
+        query = """
+            SELECT training.workout, training.description, training.breaks, completed_workouts.completed_at
+            FROM completed_workouts
+            JOIN training ON completed_workouts.training_id = training.id
+            WHERE completed_workouts.user_id = %(user_id)s
+            ORDER BY completed_workouts.completed_at DESC
+        """
+        results = connectToMySQL(cls.db).query_db(query, data)
+        completed_workouts = []
+        for row in results:
+            completed_workouts.append(cls(row))
+        return completed_workouts
