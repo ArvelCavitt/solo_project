08e50485fc1aa3531ae72ac9918bac10722087d7
Arvel Cavitt <arvelcavitt4@gmail.com>
2024-09-30 22:08:48 -0700

    commit

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -21,14 +21,14 @@ class User:
         self.location = data.get('location', None)
         self.about_me = data.get('about_me', '')
         self.goal = data.get('goal', '')
-        # adding self.profile_picture = data.get('profile_picture')
+        self.profile_picture = data.get('profile_picture')
         self.created_at  = data.get('created_at')
         self.updated_at  = data.get('updated_at')
         self.training = []
 
     @classmethod
     def save(cls,data):
-        query = "INSERT INTO user (first_name, last_name, age, email, password, location, about_me, goal) VALUES (%(first_name)s, %(last_name)s, %(age)s, %(email)s, %(password)s, %(location)s, %(about_me)s, %(goal)s)"
+        query = "INSERT INTO user (first_name, last_name, age, email, password, location, about_me, goal, profile_picture) VALUES (%(first_name)s, %(last_name)s, %(age)s, %(email)s, %(password)s, %(location)s, %(about_me)s, %(goal)s, %(profile_picture)s)"
         return connectToMySQL(cls.db).query_db(query,data)
 
     @classmethod
